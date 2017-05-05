e89dc67b8563a417ba43fec4928ec5517aee1885
Hpiz <jacobsen9026@gmail.com>
2017-05-04 21:16:55 -0400

    Order Change

--- src/main/java/hpiz/reaction/com/reaction/GameActivity.java
+++ src/main/java/hpiz/reaction/com/reaction/GameActivity.java
@@ -52,35 +52,35 @@ public class GameActivity extends Activity {
         Intent i;
         Log.v(TAG, "GameProgress=" + String.valueOf(gameProgress));
         switch (gameProgress) {
-            case 2:
+            case 3:
                 i = new Intent(GameActivity.this, SurpriseReaction.class);
                 gameProgress++;
                 Log.v(TAG, "GameProgress=" + String.valueOf(gameProgress));
                 //sp.edit().putInt("gameProgress",gameProgress).commit();
                 startActivity(i);
                 break;
-            case 1:
+            case 2:
                 i = new Intent(GameActivity.this, SurpriseNumberedReaction.class);
                 gameProgress++;
                 //sp.edit().putInt("gameProgress",gameProgress).commit();
                 startActivity(i);
                 break;
 
-            case 0:
+            case 1:
                 i = new Intent(GameActivity.this, WacAMoleReaction.class);
                 gameProgress++;
                 //sp.edit().putInt("gameProgress",gameProgress).commit();
                 startActivity(i);
                 break;
-/*
+
             case 0:
                 i = new Intent(GameActivity.this, CatchReaction.class);
                 gameProgress++;
                 //sp.edit().putInt("gameProgress",gameProgress).commit();
                 startActivity(i);
                 break;
-*/
-            case 3:
+
+            case 4:
                 i = new Intent(GameActivity.this, MainMenuActivity.class);
                 i.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
                 //sp.edit().putInt("gameProgress",gameProgress).commit();
