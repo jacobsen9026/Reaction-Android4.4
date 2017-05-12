8e8507d696e06481bb0a974d214a7ab34e480b58
Hpiz <jacobsen9026@gmail.com>
2017-05-11 22:31:09 -0400

    Color picker implementation in settings Removed fullscreen on main menu and settings

--- src/main/java/hpiz/reaction/com/reaction/MainMenuActivity.java
+++ src/main/java/hpiz/reaction/com/reaction/MainMenuActivity.java
@@ -9,12 +9,18 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.constraint.ConstraintLayout;
+import android.util.Log;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.animation.AccelerateInterpolator;
 import android.view.animation.DecelerateInterpolator;
+import android.widget.AdapterView;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.ScrollView;
+import android.widget.Spinner;
 import android.widget.TextView;
+
 
 /**
  * An example full-screen activity that shows and hides the system UI (i.e.
@@ -33,9 +39,14 @@ public class MainMenuActivity extends Activity {
     private SharedPreferences sp;
     private ValueAnimator buttonAnimator;
     private Button discardButton;
+    private String topColor;
+    private String bottomColor;
     private Button saveButton;
     private ValueAnimator backgroundAnimator;
     private ValueAnimator backgroundAnimator2;
+    private ScrollView contentContainer2;
+    private Spinner spinner;
+    private Spinner spin2;
 
 
     /**
@@ -53,13 +64,12 @@ public class MainMenuActivity extends Activity {
     }
 
 */
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.main_menu_activity);
-        hide();
+        mm_hide();
         initializeObjects();
         configureObjects();
 
@@ -111,7 +121,7 @@ public class MainMenuActivity extends Activity {
                         //Intent i = new Intent(MainMenuActivity.this, SettingsActivity.class);
                         //startActivity(i);
                         setContentView(R.layout.activity_settings);
-                        hide();
+                        s_hide();
                         initializeSettingsObjects();
                         configureSettingsObjects();
                         animateShowSettings();
@@ -188,6 +198,7 @@ public class MainMenuActivity extends Activity {
     }
 
     private void animateExitSettings() {
+        saveSettings();
         ValueAnimator settingsAnimator = ValueAnimator.ofFloat(0, 4000);
         settingsAnimator.setDuration(400);
         settingsAnimator.setInterpolator(new DecelerateInterpolator());
@@ -199,7 +210,7 @@ public class MainMenuActivity extends Activity {
 
 
                 float value = (float) animation.getAnimatedValue();
-                contentContainer.setTranslationY(value);
+                contentContainer2.setTranslationY(value);
 
 
             }
@@ -217,7 +228,7 @@ public class MainMenuActivity extends Activity {
                 //startActivity(i);
 
                 setContentView(R.layout.main_menu_activity);
-                hide();
+                mm_hide();
                 initializeObjects();
                 configureObjects();
                 buttonAnimator = ValueAnimator.ofFloat(4000, 0);
@@ -279,50 +290,6 @@ public class MainMenuActivity extends Activity {
     }
 
 
-    private void animateShowSettings() {
-        ValueAnimator settingsAnimator = ValueAnimator.ofFloat(4000, 0);
-        settingsAnimator.setDuration(400);
-        settingsAnimator.setInterpolator(new DecelerateInterpolator());
-
-        settingsAnimator.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
-
-            @Override
-            public void onAnimationUpdate(ValueAnimator animation) {
-
-
-                float value = (float) animation.getAnimatedValue();
-                contentContainer.setTranslationY(value);
-
-
-            }
-        });
-        settingsAnimator.addListener(new Animator.AnimatorListener() {
-            @Override
-            public void onAnimationStart(Animator animation) {
-
-
-            }
-
-            @Override
-            public void onAnimationEnd(Animator animation) {
-                //Intent i = new Intent(MainMenuActivity.this, SettingsActivity.class);
-                //startActivity(i);
-
-            }
-
-            @Override
-            public void onAnimationCancel(Animator animation) {
-
-            }
-
-            @Override
-            public void onAnimationRepeat(Animator animation) {
-
-            }
-        });
-        settingsAnimator.start();
-    }
-
     private void animateExitGame() {
         ValueAnimator settingsAnimator = ValueAnimator.ofFloat(1, 0);
         settingsAnimator.setDuration(300);
@@ -367,6 +334,129 @@ public class MainMenuActivity extends Activity {
         settingsAnimator.start();
     }
 
+
+    private void initializeObjects() {
+        startGameButton = (Button) findViewById(R.id.startGameButton);
+        settingsButton = (Button) findViewById(R.id.settingsButton);
+        quitGameButton = (Button) findViewById(R.id.quitGameButton);
+        mainMenuTitle = (TextView) findViewById(R.id.main_menu_title);
+        backgroundImage = (ImageView) findViewById(R.id.main_menu_background_image);
+        backgroundImageHidden = (ImageView) findViewById(R.id.main_menu_background_image_animate);
+        sp = getSharedPreferences("runningPreferences", Context.MODE_PRIVATE);
+//refreshBackground();
+
+    }
+
+    @Override
+    protected void onPostCreate(Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
+    }
+
+
+    private void mm_hide() {
+        // Hide UI first
+
+        ActionBar actionBar = getActionBar();
+        if (actionBar != null) {
+            actionBar.hide();
+        }
+
+        //mControlsView.setVisibility(View.GONE);
+        //mVisible = false;
+
+        // Schedule a runnable to remove the status and navigation bar after a delay
+        contentContainer = (ConstraintLayout) findViewById(R.id.contentContainer);
+        /*
+        contentContainer.setSystemUiVisibility(View.SYSTEM_UI_FLAG_LOW_PROFILE
+
+                | View.SYSTEM_UI_FLAG_FULLSCREEN
+                | View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+                | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION);
+                */
+
+    }
+
+    private void s_hide() {
+        // Hide UI first
+
+        ActionBar actionBar = getActionBar();
+        if (actionBar != null) {
+            actionBar.hide();
+        }
+
+        //mControlsView.setVisibility(View.GONE);
+        //mVisible = false;
+
+        // Schedule a runnable to remove the status and navigation bar after a delay
+        contentContainer2 = (ScrollView) findViewById(R.id.contentContainer2);
+        /*
+        contentContainer2.setSystemUiVisibility(View.SYSTEM_UI_FLAG_LOW_PROFILE
+
+                | View.SYSTEM_UI_FLAG_FULLSCREEN
+                | View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+                | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION);
+*/
+    }
+
+    private void loadSettings() {
+        topColor = sp.getString("topColor", "#BB3500");
+        bottomColor = sp.getString("bottomColor", "#3D5B7E");
+        spinner.setSelection(matchTopColor(topColor));
+        spin2.setSelection(matchBottomColor(bottomColor));
+
+    }
+
+    private int matchTopColor(String topColor) {
+        int index = -1;
+        int i = 0;
+        for (String c : getResources().getStringArray(R.array.topColors)) {
+            Log.v("Match Colors", c + " " + topColor);
+            if (topColor.equals(c)) {
+                index = i;
+                Log.v("Match Colors", c + " " + topColor + String.valueOf(i));
+            }
+            i++;
+        }
+        return index;
+    }
+
+
+    private int matchBottomColor(String topColor) {
+        int index = -1;
+        int i = 0;
+        for (String c : getResources().getStringArray(R.array.bottomColors)) {
+            Log.v("Match Colors", c + " " + topColor);
+            if (topColor.equals(c)) {
+                index = i;
+                Log.v("Match Colors", c + " " + topColor);
+            }
+            i++;
+        }
+        return index;
+    }
+
+    private void saveSettings() {
+        sp.edit()
+                .putString("topColor", topColor)
+                .putString("bottomColor", bottomColor)
+                .apply();
+
+    }
+
+    private void initializeSettingsObjects() {
+        saveButton = (Button) findViewById(R.id.saveButton);
+        discardButton = (Button) findViewById(R.id.discardButton);
+        backgroundImage = (ImageView) findViewById(R.id.main_menu_background_image);
+        backgroundImageHidden = (ImageView) findViewById(R.id.main_menu_background_image_animate);
+        contentContainer2 = (ScrollView) findViewById(R.id.contentContainer2);
+        spinner = (Spinner) findViewById(R.id.spinner);
+        spin2 = (Spinner) findViewById(R.id.spinner2);
+    }
+
     private void configureSettingsObjects() {
         //contentContainer.setAlpha(0);
         backgroundImage.setMinimumWidth(screenWidth);
@@ -388,53 +478,88 @@ public class MainMenuActivity extends Activity {
 
     }
 
-    private void initializeSettingsObjects() {
-        saveButton = (Button) findViewById(R.id.saveButton);
-        discardButton = (Button) findViewById(R.id.discardButton);
-        backgroundImage = (ImageView) findViewById(R.id.main_menu_background_image);
-        backgroundImageHidden = (ImageView) findViewById(R.id.main_menu_background_image_animate);
-        contentContainer = (ConstraintLayout) findViewById(R.id.contentContainer);
+    private void animateShowSettings() {
+        setupColorPickers();
+        loadSettings();
+        ValueAnimator settingsAnimator = ValueAnimator.ofFloat(4000, 0);
+        settingsAnimator.setDuration(400);
+        settingsAnimator.setInterpolator(new DecelerateInterpolator());
+
+        settingsAnimator.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
+
+            @Override
+            public void onAnimationUpdate(ValueAnimator animation) {
+
+
+                float value = (float) animation.getAnimatedValue();
+                contentContainer2.setTranslationY(value);
+
+
+            }
+        });
+        settingsAnimator.addListener(new Animator.AnimatorListener() {
+            @Override
+            public void onAnimationStart(Animator animation) {
+
+
+            }
+
+            @Override
+            public void onAnimationEnd(Animator animation) {
+                //Intent i = new Intent(MainMenuActivity.this, SettingsActivity.class);
+                //startActivity(i);
+
+            }
+
+            @Override
+            public void onAnimationCancel(Animator animation) {
+
+            }
+
+            @Override
+            public void onAnimationRepeat(Animator animation) {
+
+            }
+        });
+        settingsAnimator.start();
     }
 
-    private void initializeObjects() {
-        startGameButton = (Button) findViewById(R.id.startGameButton);
-        settingsButton = (Button) findViewById(R.id.settingsButton);
-        quitGameButton = (Button) findViewById(R.id.quitGameButton);
-        mainMenuTitle = (TextView) findViewById(R.id.main_menu_title);
-        backgroundImage = (ImageView) findViewById(R.id.main_menu_background_image);
-        backgroundImageHidden = (ImageView) findViewById(R.id.main_menu_background_image_animate);
-        sp = getSharedPreferences("runningPreferences", Context.MODE_PRIVATE);
-//refreshBackground();
+    private void setupColorPickers() {
 
+
+        spinner.setAdapter(new ColorSpinnerAdapter(this, true));
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                topColor = getResources().getStringArray(R.array.topColors)[position];
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
+        spin2.setAdapter(new ColorSpinnerAdapter(this, false));
+        spin2.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                bottomColor = getResources().getStringArray(R.array.bottomColors)[position];
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
+        spinner.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                s_hide();
+                return false;
+            }
+        });
     }
-
-    @Override
-    protected void onPostCreate(Bundle savedInstanceState) {
-        super.onPostCreate(savedInstanceState);
-    }
-
-
-    private void hide() {
-        // Hide UI first
-
-        ActionBar actionBar = getActionBar();
-        if (actionBar != null) {
-            actionBar.hide();
-        }
-
-        //mControlsView.setVisibility(View.GONE);
-        //mVisible = false;
-
-        // Schedule a runnable to remove the status and navigation bar after a delay
-        contentContainer = (ConstraintLayout) findViewById(R.id.contentContainer);
-        contentContainer.setSystemUiVisibility(View.SYSTEM_UI_FLAG_LOW_PROFILE
-
-                | View.SYSTEM_UI_FLAG_FULLSCREEN
-                | View.SYSTEM_UI_FLAG_LAYOUT_STABLE
-                | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
-                | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
-                | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION);
-
-    }
-
 }
+
+
+
