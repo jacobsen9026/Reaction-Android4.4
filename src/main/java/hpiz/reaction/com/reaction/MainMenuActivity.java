735aee4a5c43751bf64a4bad8e430a512e11bdfb
Chris Jacobsen Work <cjacobsen@work.com>
2017-05-12 09:16:29 -0400

    Resegregate Settings and Main Menu with   @Override     public void onPause() {         super.onPause();         overridePendingTransition(0, 0);     }

--- src/main/java/hpiz/reaction/com/reaction/MainMenuActivity.java
+++ src/main/java/hpiz/reaction/com/reaction/MainMenuActivity.java
@@ -9,16 +9,11 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.constraint.ConstraintLayout;
-import android.util.Log;
-import android.view.MotionEvent;
 import android.view.View;
 import android.view.animation.AccelerateInterpolator;
-import android.view.animation.DecelerateInterpolator;
-import android.widget.AdapterView;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.ScrollView;
-import android.widget.Spinner;
 import android.widget.TextView;
 
 
@@ -45,8 +40,6 @@ public class MainMenuActivity extends Activity {
     private ValueAnimator backgroundAnimator;
     private ValueAnimator backgroundAnimator2;
     private ScrollView contentContainer2;
-    private Spinner spinner;
-    private Spinner spin2;
 
 
     /**
@@ -64,6 +57,13 @@ public class MainMenuActivity extends Activity {
     }
 
 */
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        overridePendingTransition(0, 0);
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -72,7 +72,61 @@ public class MainMenuActivity extends Activity {
         mm_hide();
         initializeObjects();
         configureObjects();
+        animateShowMainMenu();
 
+
+    }
+
+    private void animateShowMainMenu() {
+
+        buttonAnimator = ValueAnimator.ofFloat(4000, 0);
+        buttonAnimator.setDuration(500);
+        buttonAnimator.setInterpolator(new AccelerateInterpolator());
+
+        buttonAnimator.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
+
+            @Override
+            public void onAnimationUpdate(ValueAnimator animation) {
+
+
+                float value = (float) animation.getAnimatedValue();
+                settingsButton.setTranslationX(value);
+                startGameButton.setTranslationX(-value);
+                quitGameButton.setTranslationY(value);
+                mainMenuTitle.setTranslationY(-value);
+
+            }
+        });
+        buttonAnimator.addListener(new Animator.AnimatorListener() {
+            @Override
+            public void onAnimationStart(Animator animation) {
+
+                settingsButton.bringToFront();
+            }
+
+            @Override
+            public void onAnimationEnd(Animator animation) {
+
+                        /*
+                        setContentView(R.layout.activity_settings);
+                        s_hide();
+                        initializeSettingsObjects();
+                        configureSettingsObjects();
+                        animateShowSettings();
+                        */
+            }
+
+            @Override
+            public void onAnimationCancel(Animator animation) {
+
+            }
+
+            @Override
+            public void onAnimationRepeat(Animator animation) {
+
+            }
+        });
+        buttonAnimator.start();
 
     }
 
@@ -90,55 +144,7 @@ public class MainMenuActivity extends Activity {
         settingsButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
-                buttonAnimator = ValueAnimator.ofFloat(0, 4000);
-                buttonAnimator.setDuration(500);
-                buttonAnimator.setInterpolator(new AccelerateInterpolator());
-
-                buttonAnimator.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
-
-                    @Override
-                    public void onAnimationUpdate(ValueAnimator animation) {
-
-
-                        float value = (float) animation.getAnimatedValue();
-                        settingsButton.setTranslationX(value);
-                        startGameButton.setTranslationX(-value);
-                        quitGameButton.setTranslationY(value);
-                        mainMenuTitle.setTranslationY(-value);
-
-                    }
-                });
-                buttonAnimator.addListener(new Animator.AnimatorListener() {
-                    @Override
-                    public void onAnimationStart(Animator animation) {
-
-                        settingsButton.bringToFront();
-                    }
-
-                    @Override
-                    public void onAnimationEnd(Animator animation) {
-                        //Intent i = new Intent(MainMenuActivity.this, SettingsActivity.class);
-                        //startActivity(i);
-                        setContentView(R.layout.activity_settings);
-                        s_hide();
-                        initializeSettingsObjects();
-                        configureSettingsObjects();
-                        animateShowSettings();
-                    }
-
-                    @Override
-                    public void onAnimationCancel(Animator animation) {
-
-                    }
-
-                    @Override
-                    public void onAnimationRepeat(Animator animation) {
-
-                    }
-                });
-                buttonAnimator.start();
-
+                animateHideMainMenu();
             }
         });
         startGameButton.setOnClickListener(new View.OnClickListener() {
@@ -197,83 +203,44 @@ public class MainMenuActivity extends Activity {
         });
     }
 
-    private void animateExitSettings() {
-        saveSettings();
-        ValueAnimator settingsAnimator = ValueAnimator.ofFloat(0, 4000);
-        settingsAnimator.setDuration(400);
-        settingsAnimator.setInterpolator(new DecelerateInterpolator());
+    private void animateHideMainMenu() {
 
-        settingsAnimator.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
+        buttonAnimator = ValueAnimator.ofFloat(0, 4000);
+        buttonAnimator.setDuration(500);
+        buttonAnimator.setInterpolator(new AccelerateInterpolator());
+
+        buttonAnimator.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
 
             @Override
             public void onAnimationUpdate(ValueAnimator animation) {
 
 
                 float value = (float) animation.getAnimatedValue();
-                contentContainer2.setTranslationY(value);
-
+                settingsButton.setTranslationX(value);
+                startGameButton.setTranslationX(-value);
+                quitGameButton.setTranslationY(value);
+                mainMenuTitle.setTranslationY(-value);
 
             }
         });
-        settingsAnimator.addListener(new Animator.AnimatorListener() {
+        buttonAnimator.addListener(new Animator.AnimatorListener() {
             @Override
             public void onAnimationStart(Animator animation) {
 
-
+                settingsButton.bringToFront();
             }
 
             @Override
             public void onAnimationEnd(Animator animation) {
-                //Intent i = new Intent(MainMenuActivity.this, SettingsActivity.class);
-                //startActivity(i);
-
-                setContentView(R.layout.main_menu_activity);
-                mm_hide();
-                initializeObjects();
-                configureObjects();
-                buttonAnimator = ValueAnimator.ofFloat(4000, 0);
-                buttonAnimator.setDuration(400);
-                buttonAnimator.setInterpolator(new DecelerateInterpolator());
-
-                buttonAnimator.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
-
-                    @Override
-                    public void onAnimationUpdate(ValueAnimator animation) {
-
-
-                        float value = (float) animation.getAnimatedValue();
-                        settingsButton.setTranslationX(value);
-                        startGameButton.setTranslationX(-value);
-                        quitGameButton.setTranslationY(value);
-                        mainMenuTitle.setTranslationY(-value);
-
-                    }
-                });
-                buttonAnimator.addListener(new Animator.AnimatorListener() {
-                    @Override
-                    public void onAnimationStart(Animator animation) {
-
-
-                    }
-
-                    @Override
-                    public void onAnimationEnd(Animator animation) {
-                        //Intent i = new Intent(MainMenuActivity.this, SettingsActivity.class);
-                        //startActivity(i);
-
-                    }
-
-                    @Override
-                    public void onAnimationCancel(Animator animation) {
-
-                    }
-
-                    @Override
-                    public void onAnimationRepeat(Animator animation) {
-
-                    }
-                });
-                buttonAnimator.start();
+                Intent i = new Intent(MainMenuActivity.this, SettingsActivity.class);
+                startActivity(i);
+                        /*
+                        setContentView(R.layout.activity_settings);
+                        s_hide();
+                        initializeSettingsObjects();
+                        configureSettingsObjects();
+                        animateShowSettings();
+                        */
             }
 
             @Override
@@ -286,7 +253,17 @@ public class MainMenuActivity extends Activity {
 
             }
         });
-        settingsAnimator.start();
+        buttonAnimator.start();
+
+    }
+
+
+    private void saveSettings() {
+        sp.edit()
+                .putString("topColor", topColor)
+                .putString("bottomColor", bottomColor)
+                .apply();
+
     }
 
 
@@ -378,187 +355,22 @@ public class MainMenuActivity extends Activity {
 
     }
 
-    private void s_hide() {
-        // Hide UI first
-
-        ActionBar actionBar = getActionBar();
-        if (actionBar != null) {
-            actionBar.hide();
-        }
-
-        //mControlsView.setVisibility(View.GONE);
-        //mVisible = false;
-
-        // Schedule a runnable to remove the status and navigation bar after a delay
-        contentContainer2 = (ScrollView) findViewById(R.id.contentContainer2);
-        /*
-        contentContainer2.setSystemUiVisibility(View.SYSTEM_UI_FLAG_LOW_PROFILE
-
-                | View.SYSTEM_UI_FLAG_FULLSCREEN
-                | View.SYSTEM_UI_FLAG_LAYOUT_STABLE
-                | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
-                | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
-                | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION);
-*/
-    }
-
-    private void loadSettings() {
-        topColor = sp.getString("topColor", "#BB3500");
-        bottomColor = sp.getString("bottomColor", "#3D5B7E");
-        spinner.setSelection(matchTopColor(topColor));
-        spin2.setSelection(matchBottomColor(bottomColor));
-
-    }
-
-    private int matchTopColor(String topColor) {
-        int index = -1;
-        int i = 0;
-        for (String c : getResources().getStringArray(R.array.topColors)) {
-            Log.v("Match Colors", c + " " + topColor);
-            if (topColor.equals(c)) {
-                index = i;
-                Log.v("Match Colors", c + " " + topColor + String.valueOf(i));
-            }
-            i++;
-        }
-        return index;
-    }
 
 
-    private int matchBottomColor(String topColor) {
-        int index = -1;
-        int i = 0;
-        for (String c : getResources().getStringArray(R.array.bottomColors)) {
-            Log.v("Match Colors", c + " " + topColor);
-            if (topColor.equals(c)) {
-                index = i;
-                Log.v("Match Colors", c + " " + topColor);
-            }
-            i++;
-        }
-        return index;
-    }
-
-    private void saveSettings() {
-        sp.edit()
-                .putString("topColor", topColor)
-                .putString("bottomColor", bottomColor)
-                .apply();
-
-    }
-
-    private void initializeSettingsObjects() {
-        saveButton = (Button) findViewById(R.id.saveButton);
-        discardButton = (Button) findViewById(R.id.discardButton);
-        backgroundImage = (ImageView) findViewById(R.id.main_menu_background_image);
-        backgroundImageHidden = (ImageView) findViewById(R.id.main_menu_background_image_animate);
-        contentContainer2 = (ScrollView) findViewById(R.id.contentContainer2);
-        spinner = (Spinner) findViewById(R.id.spinner);
-        spin2 = (Spinner) findViewById(R.id.spinner2);
-    }
-
-    private void configureSettingsObjects() {
-        //contentContainer.setAlpha(0);
-        backgroundImage.setMinimumWidth(screenWidth);
-        backgroundImageHidden.setMinimumWidth(screenWidth);
-        backgroundImage.setMinimumHeight(screenHeight);
-        backgroundImageHidden.setMinimumHeight(screenHeight);
-        saveButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                animateExitSettings();
-            }
-        });
-        discardButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                animateExitSettings();
-            }
-        });
-
-    }
-
-    private void animateShowSettings() {
-        setupColorPickers();
-        loadSettings();
-        ValueAnimator settingsAnimator = ValueAnimator.ofFloat(4000, 0);
-        settingsAnimator.setDuration(400);
-        settingsAnimator.setInterpolator(new DecelerateInterpolator());
-
-        settingsAnimator.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
-
-            @Override
-            public void onAnimationUpdate(ValueAnimator animation) {
 
 
-                float value = (float) animation.getAnimatedValue();
-                contentContainer2.setTranslationY(value);
 
 
-            }
-        });
-        settingsAnimator.addListener(new Animator.AnimatorListener() {
-            @Override
-            public void onAnimationStart(Animator animation) {
 
 
-            }
-
-            @Override
-            public void onAnimationEnd(Animator animation) {
-                //Intent i = new Intent(MainMenuActivity.this, SettingsActivity.class);
-                //startActivity(i);
-
-            }
-
-            @Override
-            public void onAnimationCancel(Animator animation) {
-
-            }
-
-            @Override
-            public void onAnimationRepeat(Animator animation) {
-
-            }
-        });
-        settingsAnimator.start();
-    }
-
-    private void setupColorPickers() {
 
 
-        spinner.setAdapter(new ColorSpinnerAdapter(this, true));
-        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
-            @Override
-            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-                topColor = getResources().getStringArray(R.array.topColors)[position];
-            }
 
-            @Override
-            public void onNothingSelected(AdapterView<?> parent) {
 
-            }
-        });
-        spin2.setAdapter(new ColorSpinnerAdapter(this, false));
-        spin2.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
-            @Override
-            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-                bottomColor = getResources().getStringArray(R.array.bottomColors)[position];
-            }
 
-            @Override
-            public void onNothingSelected(AdapterView<?> parent) {
 
-            }
-        });
-        spinner.setOnTouchListener(new View.OnTouchListener() {
-            @Override
-            public boolean onTouch(View v, MotionEvent event) {
-                s_hide();
-                return false;
-            }
-        });
-    }
+
+
 }
 
 
--- src/main/java/hpiz/reaction/com/reaction/SettingsActivity.java
+++ src/main/java/hpiz/reaction/com/reaction/SettingsActivity.java
@@ -1,12 +1,21 @@
 package hpiz.reaction.com.reaction;
 
+import android.animation.Animator;
+import android.animation.ValueAnimator;
 import android.app.ActionBar;
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
-import android.support.constraint.ConstraintLayout;
+import android.util.Log;
+import android.view.MotionEvent;
 import android.view.View;
+import android.view.animation.DecelerateInterpolator;
+import android.widget.AdapterView;
 import android.widget.Button;
+import android.widget.ScrollView;
+import android.widget.Spinner;
 
 /**
  * An example full-screen activity that shows and hides the system UI (i.e.
@@ -16,7 +25,13 @@ public class SettingsActivity extends Activity {
     private final String TAG = "com.reaction.hpiz.SettingsActivity";
     private Button discardButton;
     private Button saveButton;
-    private ConstraintLayout contentContainer;
+    private ScrollView contentContainer;
+    private SharedPreferences sp;
+    private String topColor;
+    private String bottomColor;
+    private Spinner spinner;
+    private Spinner spin2;
+
 
     /**
      * Touch listener to use for in-layout UI controls to delay hiding the
@@ -34,6 +49,7 @@ public class SettingsActivity extends Activity {
         initializeObjects();
         configureObjects();
         hide();
+        animateShowSettings();
 
     }
 
@@ -41,18 +57,13 @@ public class SettingsActivity extends Activity {
         saveButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent i = new Intent(SettingsActivity.this, MainMenuActivity.class);
-                i.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
-                startActivity(i);
+                animateExitSettings();
             }
         });
         discardButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent i = new Intent(SettingsActivity.this, MainMenuActivity.class);
-                i.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
-
-                startActivity(i);
+                animateExitSettings();
             }
         });
 
@@ -61,13 +72,194 @@ public class SettingsActivity extends Activity {
     private void initializeObjects() {
         saveButton = (Button) findViewById(R.id.saveButton);
         discardButton = (Button) findViewById(R.id.discardButton);
+        sp = getSharedPreferences("runningPreferences", Context.MODE_PRIVATE);
+        spin2 = (Spinner) findViewById(R.id.spinner2);
+        spinner = (Spinner) findViewById(R.id.spinner);
     }
 
+    @Override
+    public void onPause() {
+        super.onPause();
+        overridePendingTransition(0, 0);
+    }
     @Override
     protected void onPostCreate(Bundle savedInstanceState) {
         super.onPostCreate(savedInstanceState);
     }
 
+    private void animateShowSettings() {
+        setupColorPickers();
+        loadSettings();
+        ValueAnimator settingsAnimator = ValueAnimator.ofFloat(4000, 0);
+        settingsAnimator.setDuration(400);
+        settingsAnimator.setInterpolator(new DecelerateInterpolator());
+
+        settingsAnimator.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
+
+            @Override
+            public void onAnimationUpdate(ValueAnimator animation) {
+
+
+                float value = (float) animation.getAnimatedValue();
+                contentContainer.setTranslationY(value);
+
+
+            }
+        });
+        settingsAnimator.addListener(new Animator.AnimatorListener() {
+            @Override
+            public void onAnimationStart(Animator animation) {
+
+
+            }
+
+            @Override
+            public void onAnimationEnd(Animator animation) {
+                //Intent i = new Intent(MainMenuActivity.this, SettingsActivity.class);
+                //startActivity(i);
+
+            }
+
+            @Override
+            public void onAnimationCancel(Animator animation) {
+
+            }
+
+            @Override
+            public void onAnimationRepeat(Animator animation) {
+
+            }
+        });
+        settingsAnimator.start();
+    }
+
+    private void animateExitSettings() {
+        saveSettings();
+        ValueAnimator settingsAnimator = ValueAnimator.ofFloat(0, 4000);
+        settingsAnimator.setDuration(400);
+        settingsAnimator.setInterpolator(new DecelerateInterpolator());
+
+        settingsAnimator.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
+
+            @Override
+            public void onAnimationUpdate(ValueAnimator animation) {
+
+
+                float value = (float) animation.getAnimatedValue();
+                contentContainer.setTranslationY(value);
+
+
+            }
+        });
+        settingsAnimator.addListener(new Animator.AnimatorListener() {
+            @Override
+            public void onAnimationStart(Animator animation) {
+
+
+            }
+
+            @Override
+            public void onAnimationEnd(Animator animation) {
+                //Intent i = new Intent(MainMenuActivity.this, SettingsActivity.class);
+                //startActivity(i);
+                Intent i = new Intent(SettingsActivity.this, MainMenuActivity.class);
+                i.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                startActivity(i);
+
+            }
+
+            @Override
+            public void onAnimationCancel(Animator animation) {
+
+            }
+
+            @Override
+            public void onAnimationRepeat(Animator animation) {
+
+            }
+        });
+        settingsAnimator.start();
+    }
+
+    private void loadSettings() {
+        topColor = sp.getString("topColor", "#BB3500");
+        bottomColor = sp.getString("bottomColor", "#3D5B7E");
+        spinner.setSelection(matchTopColor(topColor));
+        spin2.setSelection(matchBottomColor(bottomColor));
+
+    }
+
+    private int matchTopColor(String topColor) {
+        int index = -1;
+        int i = 0;
+        for (String c : getResources().getStringArray(R.array.topColors)) {
+            Log.v("Match Colors", c + " " + topColor);
+            if (topColor.equals(c)) {
+                index = i;
+                Log.v("Match Colors", c + " " + topColor + String.valueOf(i));
+            }
+            i++;
+        }
+        return index;
+    }
+
+    private int matchBottomColor(String topColor) {
+        int index = -1;
+        int i = 0;
+        for (String c : getResources().getStringArray(R.array.bottomColors)) {
+            Log.v("Match Colors", c + " " + topColor);
+            if (topColor.equals(c)) {
+                index = i;
+                Log.v("Match Colors", c + " " + topColor);
+            }
+            i++;
+        }
+        return index;
+    }
+
+    private void setupColorPickers() {
+
+
+        spinner.setAdapter(new ColorSpinnerAdapter(this, true));
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                topColor = getResources().getStringArray(R.array.topColors)[position];
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
+        spin2.setAdapter(new ColorSpinnerAdapter(this, false));
+        spin2.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                bottomColor = getResources().getStringArray(R.array.bottomColors)[position];
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
+        spinner.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                //s_hide();
+                return false;
+            }
+        });
+    }
+
+    private void saveSettings() {
+        sp.edit()
+                .putString("topColor", topColor)
+                .putString("bottomColor", bottomColor)
+                .apply();
+
+    }
 
     private void hide() {
         // Hide UI first
@@ -78,7 +270,8 @@ public class SettingsActivity extends Activity {
             actionBar.hide();
         }
         // Schedule a runnable to remove the status and navigation bar after a delay
-        contentContainer = (ConstraintLayout) findViewById(R.id.contentContainer);
+        contentContainer = (ScrollView) findViewById(R.id.contentContainer2);
+        /*
         contentContainer.setSystemUiVisibility(View.SYSTEM_UI_FLAG_LOW_PROFILE
 
                 | View.SYSTEM_UI_FLAG_FULLSCREEN
@@ -86,7 +279,7 @@ public class SettingsActivity extends Activity {
                 | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
                 | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
                 | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION);
-
+*/
     }
 
 }
